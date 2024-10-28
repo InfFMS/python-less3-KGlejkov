2cdd3c8fe9c834f53de8e4bdae1294e04912264e
Gleb <gklejkov@gmail.com>
2024-10-28 09:06:54 +0700

    решена задача 6

--- task6.py
+++ task6.py
@@ -1,3 +1,13 @@
 # с клавиатуры вводятся числа, ввод завершается числом 0.
 # Определить минимальное и максимальное из введённых чисел.
-
+mas=[]
+while True:
+    x=int(input())
+    if x==0:
+          break
+    else:
+        mas.append(x)
+if mas==[]:
+    print("")
+else:
+    print(min(mas), max(mas))
