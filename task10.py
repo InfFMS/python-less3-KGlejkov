8358ff38a093cdc1554a052f55ea910ecbda3f3f
Gleb <gklejkov@gmail.com>
2024-10-28 09:17:53 +0700

    решена 10 задача

--- task10.py
+++ task10.py
@@ -2,6 +2,22 @@
 # Определить минимальное и максимальное среди простых чисел
 # (которые делятся на сами не себя и на 1).
 # Если таких чисел не было, вывести "нет".
-
+def f(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+mas=[]
+N=int(input())
+for i in range(N):
+    x=int(input())
+    if f(x):
+        mas.append(x)
+if mas==[]:
+    print("no")
+else:
+    print(min(mas),max(mas))
 
 
